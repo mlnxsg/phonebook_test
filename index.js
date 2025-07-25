8cda531e6f0f39cdd227036fb8dd06fad0887df7
mlnxsg <1727091819@qq.com>
2025-07-25 16:26:58 +0800

    wip

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const cors = require('cors')
 
 app.use(express.json())
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(cors())
 app.use(express.static('dist'))
 
 let persons = [
